8e096b39dfb58c7cdcb63379138565bcf79445eb
64160271 <118897380+64160271@users.noreply.github.com>
2023-08-18 21:57:33 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="h-screen bg-base-200 pt-3">
+    <Login v-if="['login'].includes($route.name)"/>
+    <LayoutSidebar v-if="!['login'].includes($route.name)" />
+    <div id="" class="px-4 sm:ml-64" v-if="!['login'].includes($route.name)">
+      <div class="fixed w-5/6 rounded-box bg-white h-screen">
+         <div class="m-5">
+            <router-view />
+         </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import LayoutSidebar from './components/layouts/LayoutSidebar.vue';
+  import Login from './components/Login.vue'
+  import { ref, onMounted } from 'vue';
+  import axios from 'axios'
+
+  /* const universities = ref({})
+
+  const getUniversities = async() => {
+    await axios.get(`${import.meta.env.VITE_API_HOST}/university`)
+      .then((response) => {
+          universities.value = response.data
+      })
+  }
+
+  onMounted(() => getUniversities()) */
+</script>
+
+<style scoped>
+
+</style>
