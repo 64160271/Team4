9613fc4cace370dc9c7d206703e7dc02e7a5945f
64160299 <64160299@go.buu.ac.th>
2023-09-21 03:11:39 +0700

    Edit intern
    พยยามทำให้สามารถแก้ไขได้ด้วย LOC ที่น้อยที่สุด แต่ไม่สามารถ ต้องขออภัย

---
+++ src/stores/constData.js
@@ -0,0 +1,56 @@
+import { defineStore } from "pinia";
+
+export const useStatusData = defineStore('status', {
+    state: () => {
+        return [
+            'กำลังทำงาน',
+            'พ้นสภาพ'
+        ]
+    }
+})
+
+export const useInternType = defineStore('internType', {
+    state: () => {
+        return [
+            'นักศึกษาฝึกงาน',
+            'พนักงานประจำ',
+            'พนักงานพาร์ทไทม์'
+        ]
+    }
+})
+
+export const useGenderData = defineStore('gender', {
+    state: () => {
+        return [
+            'ชาย',
+            'หญิง',
+            'ไม่ระบุ'
+        ]
+    }
+})
+
+export const useMilitaryStatus = defineStore('militaryStatus', {
+    state: () => {
+        return [
+            'ยังไม่เกณฑ์ทหาร',
+            'กำลังประจำกองทหาร',
+            'ผ่านการเกณฑ์ทหารมาแล้ว',
+            'ได้รับการยกเว้นการเกณฑ์ทหาร'
+        ]
+    }
+})
+
+export const useMartialStatus = defineStore('martialStatus', {
+    state: () => {
+        return [
+            'โสด',
+            'มีแฟนแล้ว',
+            'หมั้น',
+            'สมรส',
+            'แยกกันอยู่',
+            'หย่าร้าง',
+            'หม้าย',
+            'ซับซ้อน'
+        ]
+    }
+})
