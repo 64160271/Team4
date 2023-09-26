22b14320c2c678a9834a37a491fd6fbe787637c2
Teerajuk Sakunchaisitthichok <114974048+teerajuk@users.noreply.github.com>
2023-09-26 21:58:32 +0700

    Update ShowCertificate.vue

--- src/components/Certificate/ShowCertificate.vue
+++ src/components/Certificate/ShowCertificate.vue
@@ -1,6 +1,15 @@
 <template>
-    <div>
-        หน้าแสดงรายการเอกสารรับรอง
+    <div class="d-flex mb-3">
+        <div>
+            <label style="font-size: 20px; color: var(--main-color);" for="">เอกสารรับรอง</label>
+        </div>
+        <div style="margin-left: 70.5%;">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn_add  btn outline-red btn-primary">
+                ออกเอกสารรับรอง
+            </button>
+        </div>
+
     </div>
 </template>
 
@@ -9,5 +18,12 @@
 </script>
 
 <style scoped>
-
+.btn_add {
+    background-color: white;
+    border: 1px solid red;
+    border-radius: 20px;
+    color: red;
+    width: 150px;
+    height: 35px;
+}
 </style>
