2ddd1e54723a563753b9c801a6e6f2a70f4ac215
64160299 <64160299@go.buu.ac.th>
2023-12-09 04:23:09 +0700

    Create LayoutInternInfo.vue

---
+++ src/components/InternInfo/LayoutInternInfo.vue
@@ -0,0 +1,50 @@
+<template>
+
+      <div class="row mx-auto my-3" style="width: 80%">
+        <div class="col-auto">
+          <img id="blah" :src="getImage()" alt="" class="img bg-grays-200" />
+        </div>
+  
+        <div class="col ms-5 align-self-center">
+          <div class="row mb-2 ms-2">
+            <label for="" class="col-md-2 col-form-label text-gray"> รหัสนักศึกษาฝึกงาน </label>
+            <div class="col">
+              <input
+                placeholder="-"
+                :value="sectionName"
+                type="text"
+                class="form-control-plaintext"
+                readonly
+                required
+              />
+            </div>
+  
+            <label for="" class="col-md-2 col-form-label text-gray"> ชื่อ-นามสกุล </label>
+            <div class="col">
+              <input
+                placeholder="-"
+                :value="departmentName"
+                type="text"
+                class="form-control-plaintext"
+                readonly
+                required
+              />
+            </div>
+          </div>
+        </div>
+
+        <slot></slot>
+      </div>
+  </template>
+  
+  <script setup>
+
+  const props = defineProps({
+    image: [String, Boolean],
+    code: [String, Boolean],
+    name: [String, Boolean],
+  })
+  </script>
+  
+  <style scoped></style>
+  
