cd2698753950d6a54093a6a36851b2a30b0f59d6
Rawich Piboonsin <64160299@go.buu.ac.th>
2023-12-09 17:05:48 +0700

    Update LayoutInternInfo.vue

--- src/components/InternInfo/LayoutInternInfo.vue
+++ src/components/InternInfo/LayoutInternInfo.vue
@@ -1,50 +1,49 @@
 <template>
+  <div class="row mx-auto my-3" style="width: 80%">
+    <div class="col-auto">
+      <img id="blah" :src="getImage()" alt="" class="img bg-grays-200" />
+    </div>
 
-      <div class="row mx-auto my-3" style="width: 80%">
-        <div class="col-auto">
-          <img id="blah" :src="getImage()" alt="" class="img bg-grays-200" />
-        </div>
-  
-        <div class="col ms-5 align-self-center">
-          <div class="row mb-2 ms-2">
-            <label for="" class="col-md-2 col-form-label text-gray"> รหัสนักศึกษาฝึกงาน </label>
-            <div class="col">
-              <input
-                placeholder="-"
-                :value="sectionName"
-                type="text"
-                class="form-control-plaintext"
-                readonly
-                required
-              />
-            </div>
-  
-            <label for="" class="col-md-2 col-form-label text-gray"> ชื่อ-นามสกุล </label>
-            <div class="col">
-              <input
-                placeholder="-"
-                :value="departmentName"
-                type="text"
-                class="form-control-plaintext"
-                readonly
-                required
-              />
-            </div>
-          </div>
+    <div class="col ms-5 align-self-center">
+      <div class="row mb-2 ms-2">
+        <label for="" class="col-md-2 col-form-label text-gray">
+          รหัสนักศึกษาฝึกงาน
+        </label>
+        <div class="col">
+          <input
+            placeholder="-"
+            :value="code"
+            type="text"
+            class="form-control-plaintext"
+            readonly
+            required
+          />
         </div>
 
-        <slot></slot>
+        <label for="" class="col-md-2 col-form-label text-gray"> ชื่อ-นามสกุล </label>
+        <div class="col">
+          <input
+            placeholder="-"
+            :value="name"
+            type="text"
+            class="form-control-plaintext"
+            readonly
+            required
+          />
+        </div>
       </div>
-  </template>
-  
-  <script setup>
+    </div>
 
-  const props = defineProps({
-    image: [String, Boolean],
-    code: [String, Boolean],
-    name: [String, Boolean],
-  })
-  </script>
-  
-  <style scoped></style>
-  
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  image: [String, Boolean],
+  code: [String, Boolean],
+  name: [String, Boolean],
+});
+</script>
+
+<style scoped></style>
