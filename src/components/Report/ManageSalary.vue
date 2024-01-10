dc25ef5d51f03f962f35bbc9db02f29223a35179
64160271 <118897380+64160271@users.noreply.github.com>
2024-01-10 23:18:19 +0700

    update datable
    เปลี่ยนจากตารางเป็น data table

--- src/components/Report/ManageSalary.vue
+++ src/components/Report/ManageSalary.vue
@@ -1,81 +1,52 @@
 <template>
     <LayoutMenuName page-name="รายงานเบี้ยเลี้ยง" />
-    <div class="row">
-        <div class="col-md-3">
-            <div class="row">
-                <div class="col-md-9">
-                    <!-- ส่วนของ radio buttons -->
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="radiotest" id="year" value="year">
-                        <label class="form-check-label" for="year">ปี</label>
-                    </div>
+    <div class="row mb-3">
+        <div class="col-auto my-auto">
+            <!-- ส่วนของ radio buttons -->
+            <div class="form-check form-check-inline">
+                <input v-model="searchData" type="text" id="search-bar" class="bg-grays-200 form-control"
+                    placeholder="Search" aria-label="" aria-describedby="basic-addon1" />
+            </div>
 
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="radiotest" id="month" value="month">
-                        <label class="form-check-label" for="month">เดือน</label>
-                    </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="radiotest" id="month" value="month">
+                <label class="form-check-label" for="month">เดือน</label>
+            </div>
 
-                    <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="radiotest" id="customized" value="customized">
-                        <label class="form-check-label" for="customized">กำหนดเอง</label>
-                    </div>
-                </div>
-                <div class="col-md-3">
-
-                    <div>
-                        <BaseButton label="เพิ่มข้อมูล" />
-                    </div>
-                </div>
+            <div class="form-check form-check-inline">
+                <BaseSelect 
+                />
+            </div>
+        </div>
+        <div class="col-auto ms-auto my-auto">
+            <div>
+                <BaseButton label="เพิ่มข้อมูล" />
             </div>
         </div>
     </div>
-    <BaseTable>
-        <tr>
-            <th>
-                รหัสรายการ
-            </th>
-            <th>
-                จำนวนรายชื่อ
-            </th>
-            <th>
-                วันที่สร้างรายการ
-            </th>
-            <th>
-                วันที่แก้ไขรายการ
-            </th>
-            <th>
-                ผู้แก้ไขสร้างรายการ
-            </th>
-            <th>
-                ข้อมูลผู้แก้ไขล่าสุด
-            </th>
-            <th>
-                สถานะ
-            </th>
-            <th>
-                แก้ไข
-            </th>
-            <th>
-                ลบ
-            </th>
-        </tr>
-        <tr>
-            <td>a</td>
-            <td>ab</td>
-            <td>bb</td>
-            <td>ad</td>
-            <td>ad</td>
-            <td>fa</td>
-            <td>af</td>
-            <td>afa</td>
-            <td>afa</td>
-        </tr>
-    </BaseTable>
+    <DataTable :heads="dataHead">
+
+    </DataTable>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import BaseButton from '../Component/BaseButton.vue'
-import BaseTable from '../Component/BaseTable.vue'
+import DataTable from '../Component/DataTable.vue'
+import BaseSelect from '../Component/BaseSelect.vue'
+
+const dataHead = ref([
+    { key: "rep_id", title: "รหัสรายการ", align: "center" },
+    { key: "rep_count_name", title: "จำนวนรายชื่อ", align: "right" },
+    { key: "rep_created_at", title: "วันที่สร้างรายการ", align: "center" },
+    { key: "rep_updated_at", title: "วันที่แก้ไขรายการ", align: "center" },
+    { key: "rep_created_user", title: "ผู้สร้างรายการ" },
+    { key: "rep_update_user", title: "ผู้แก้ไขข้อมูลล่าสุด" },
+    { key: "rep_status", title: "สถานะ", align: "center" },
+    { key: "rep_action", title: "แก้ไข / ลบ", align: "center" },
+
+])
+
 </script>
 
 <style scoped></style> 
