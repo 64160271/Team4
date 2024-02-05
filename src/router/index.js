1e8ae3f6695c0138f45adc7a402dfd51a625f16a
Rawich Piboonsin <64160299@go.buu.ac.th>
2024-02-05 16:01:05 +0700

    แย่ละ

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, createWebHashHistory } from "vue-router";
 import Index from '../components/Index.vue'
 import Login from '../components/Login.vue'
 import InternData from '../components/InternInfo/InternData.vue'
