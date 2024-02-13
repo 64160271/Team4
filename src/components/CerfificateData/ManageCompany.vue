e52866e6d8a46a86a098575249dca5d1af002d90
Rawich Piboonsin <64160299@go.buu.ac.th>
2024-02-13 11:17:56 +0700

    Revert "Merge branch 'main' into view-intern-info"
    This reverts commit 4e4905806a100a595f434a0c90d4a5910f9b1253, reversing
    changes made to b081e92fd66652cb040181c903d96dfd4a304aac.

--- src/components/CerfificateData/ManageCompany.vue
+++ src/components/CerfificateData/ManageCompany.vue
@@ -2,95 +2,10 @@
     <LayoutMenuName page-name="จัดการข้อมูลบริษัท" />
 </template>
 
-<script>
+<script setup>
 
 </script>
 
 <style scoped>
-.btn_add {
-    background-color: white;
-    border: 1px solid red;
-    border-radius: 5px;
-    color: red;
-    width: 150px;
-    height: 35px;
-    outline: none;
-}
 
-.card_sign {
-    border: 1px solid black;
-    width: 15rem;
-    height: 18rem;
-    border-radius: 20px;
-    text-align: center;
-}
-
-.card-content {
-    margin: 0rem 5px 0rem 5px;
-}
-
-.size_font_modal {
-    font-size: 16px;
-    font-weight: bold;
-    margin-top: 15px;
-    margin-left: 25%;
-}
-
-.size_input_modal {
-    border: 1px solid black;
-    border-radius: 5px;
-    margin-left: 25%;
-    margin-top: 10px;
-}
-
-.size_btn_modal {
-    border: 1px solid black;
-    border-radius: 20px;
-    width: 150px;
-    color: gray;
-    background-color: white;
-    /* margin-right: 3rem; */
-}
-
-.size_btn_modal_save {
-    border: 1px solid black;
-    border-radius: 20px;
-    width: 150px;
-    color: var(--main-color);
-    border: 1px solid var(--main-color);
-    background-color: white;
-}
-
-.size_btn_modal_save:hover {
-    border: 1px solid black;
-    border-radius: 20px;
-    width: 150px;
-    color: white;
-    border: 1px solid var(--main-color);
-    background-color: var(--main-color);
-}
-
-.stroke_edit {
-    stroke: black;
-    stroke-width: 2;
-    stroke-linecap: round;
-    stroke-linejoin: round;
-}
-
-.stroke_edit:hover {
-    stroke: var(--main-color);
-    stroke-width: 2;
-    stroke-linecap: round;
-    stroke-linejoin: round;
-}
-
-.outline-card {
-    border: 1px solid black;
-    color: black;
-}
-
-.outline-card:hover {
-    border: 1px solid var(--main-color);
-    color: var(--main-color);
-}
 </style>
--- src/components/Component/BaseCard.vue
+++ src/components/Component/BaseCard.vue
@@ -11,8 +11,6 @@
     <div class="col-md-3">
         <div class="card h-100 content border shadow-sm text-center">
             <div class="card-body">
-                <slot name="before-title"></slot>
-
                 <h6 class="card-title py-2">{{ title }}</h6>
 
                 <div class="row py-2">
@@ -24,9 +22,8 @@
                 </div>
                 
                 <div class="row py-2">
-                    <slot name="after-title"></slot>
+                    <slot></slot>
                 </div>
-
             </div>
         </div>
     </div>
