5ec527209f644b9af95ba09eb390015213f83db7
64160299 <64160299@go.buu.ac.th>
2024-02-14 21:39:12 +0700

    Revert "Update BaseCard.vue"
    This reverts commit c2a8a5f411e1ba5c0fab07c45b4a12e3aaaf268f.

--- src/components/Component/BaseCard.vue
+++ src/components/Component/BaseCard.vue
@@ -9,10 +9,8 @@
 
 <template>
     <div class="col-md-3">
-        <div :class="{ 'card-hover': hover }" class="card h-100 content border shadow-sm text-center">
+        <div class="card h-100 content border shadow-sm text-center">
             <div class="card-body">
-                <slot name="before-title"></slot>
-
                 <h6 class="card-title py-2">{{ title }}</h6>
 
                 <div class="row py-2">
@@ -23,9 +21,8 @@
                     <label>{{ content }}</label>
                 </div>            
                 <div class="row py-2">
-                    <slot name="after-title"></slot>
+                    <slot></slot>
                 </div>
-
             </div>
         </div>
     </div>
@@ -37,12 +34,11 @@ defineProps({
     image: [Boolean, String],
     sub: String,
     content: String,
-    hover: [Boolean, String],
 })
 </script>
 
 <style scoped>
-.card-hover:hover {
+.card:hover {
     border-color: var(--main-color) !important;
     /* color: var(--main-color) !important */
 }
