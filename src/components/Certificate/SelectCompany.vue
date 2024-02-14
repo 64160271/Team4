6d13c7793a5c2ff51c7887e1e04eda3f73415cd4
Rawich Piboonsin <64160299@go.buu.ac.th>
2024-02-14 13:09:06 +0700

    add section space

--- src/components/Certificate/SelectCompany.vue
+++ src/components/Certificate/SelectCompany.vue
@@ -1,89 +1,92 @@
 <template>
-    <LayoutMenuName backButton page-name="เอกสารรับรอง > บริษัท" />
+  <LayoutMenuName backButton page-name="เอกสารรับรอง > บริษัท" />
 
+  <SectionSpace>
     <div class="row mb-3 me-1">
-        <div class="col-md-5 my-auto">
-            <Search />
-        </div>
+      <div class="col-md-5 my-auto">
+        <Search />
+      </div>
     </div>
-
 
     <!-- Card -->
     <div class="row mt-3">
-        <BaseCard class="mb-4" v-for="company in companies" :title="company?.com_name" :sub="company.com_address?.addr_province"
-            :content="getAddress(company?.com_address)">
-            <template #after-title>
-                <div class="text-center">
-                    <!-- <button class="col-auto btn btn_choose" @click="sentCompanyId(company.com_id)">เลือก</button> -->
-                    <BaseButton label="เลือก" @click="sentCompanyId(company.com_id)"/>
-                </div>
-            </template>
-
-
-        </BaseCard>
+      <BaseCard
+        class="mb-4"
+        v-for="company in companies"
+        :title="company?.com_name"
+        :sub="company.com_address?.addr_province"
+        :content="getAddress(company?.com_address)"
+      >
+        <template #after-title>
+          <div class="text-center">
+            <!-- <button class="col-auto btn btn_choose" @click="sentCompanyId(company.com_id)">เลือก</button> -->
+            <BaseButton label="เลือก" @click="sentCompanyId(company.com_id)" />
+          </div>
+        </template>
+      </BaseCard>
     </div>
+  </SectionSpace>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
-import BaseCard from '../Component/BaseCard.vue';
-import BaseButton from '../Component/BaseButton.vue';
+import { ref, onMounted } from "vue";
+import axios from "axios";
+import BaseCard from "../Component/BaseCard.vue";
+import BaseButton from "../Component/BaseButton.vue";
 import router from "@/router";
-import Search from '../Component/SearchBox.vue';
+import Search from "../Component/SearchBox.vue";
 
-const companies = ref([])
+const companies = ref([]);
 
 const getCompany = async () => {
-    await axios.get(`${import.meta.env.VITE_API_HOST}/companies/address`)
-        .then((response) => {
-            companies.value = response.data
-        })
-}
+  await axios
+    .get(`${import.meta.env.VITE_API_HOST}/companies/address`)
+    .then((response) => {
+      companies.value = response.data;
+    });
+};
 
 onMounted(() => {
-    getCompany()
-})
+  getCompany();
+});
 
 function sentCompanyId(id) {
-    router.push({
-        name: 'selectSignature',
-        params: {
-            companyId: id
-        }
-    })
+  router.push({
+    name: "selectSignature",
+    params: {
+      companyId: id,
+    },
+  });
 }
 
-
 function getAddress(address) {
-    let result = `${address?.addr_house_number || ''} 
-    ${address?.addr_village_number  || ''}
-    ${address?.addr_alley  || ''}
-    ${address?.addr_street  || ''} ${address?.addr_subdistrict}
-    ${address?.addr_district} ${address?.addr_province} ${address?.addr_post_code}`
-    return result
+  let result = `${address?.addr_house_number || ""} 
+    ${address?.addr_village_number || ""}
+    ${address?.addr_alley || ""}
+    ${address?.addr_street || ""} ${address?.addr_subdistrict}
+    ${address?.addr_district} ${address?.addr_province} ${address?.addr_post_code}`;
+  return result;
 }
 </script>
 
 <style scoped>
 .back {
-    display: flex;
-    margin-right: 10px;
-    margin-bottom: 20px;
+  display: flex;
+  margin-right: 10px;
+  margin-bottom: 20px;
 }
 
 .search {
-    background-color: #8d969b30 !important;
+  background-color: #8d969b30 !important;
 }
 
 .btn_choose {
-    border: 1px solid black;
-    color: black;
-
+  border: 1px solid black;
+  color: black;
 }
 
 .btn_choose:hover {
-    border: 1px solid var(--main-color);
-    /* color: var(--main-color); */
+  border: 1px solid var(--main-color);
+  /* color: var(--main-color); */
 }
 </style>
--- src/components/Certificate/ShowCertificate.vue
+++ src/components/Certificate/ShowCertificate.vue
@@ -1,113 +1,150 @@
 <template>
-    <LayoutMenuName page-name="เอกสารรับรองการฝึกงาน" />
+  <LayoutMenuName page-name="เอกสารรับรองการฝึกงาน" />
+
+  <SectionSpace>
     <div class="row mb-3 me-1">
-        <div class="col-md-3 my-auto nopadding">
-            <SearchBox v-model="searchData" @search="search" />
-        </div>
+      <div class="col-md-3 my-auto nopadding">
+        <SearchBox v-model="searchData" @search="search" />
+      </div>
 
-        <div class="col-md-2 my-auto">
-            <BaseSelect placeholder="ทีม" all-select @change="setCurrentPage(1)" v-model="team_id" :options="teams"
-                value="team_id" text="team_name" />
-        </div>
+      <div class="col-md-2 my-auto">
+        <BaseSelect
+          placeholder="ทีม"
+          all-select
+          @change="setCurrentPage(1)"
+          v-model="team_id"
+          :options="teams"
+          value="team_id"
+          text="team_name"
+        />
+      </div>
 
-        <div class="col-md-2 my-auto nopadding">
-            <BaseInput placeholder="วันที่เริ่มต้นฝึกงาน" @change="setCurrentPage(1)" v-model="startDate"
-                onfocus="(this.type='date')" onblur="(this.type='text')" />
-        </div>
-        <BaseButton class="col-auto ms-auto" @click="$router.push('/certificates/selectCompany')" label="ออกเอกสารรับรอง" />
+      <div class="col-md-2 my-auto nopadding">
+        <BaseInput
+          placeholder="วันที่เริ่มต้นฝึกงาน"
+          @change="setCurrentPage(1)"
+          v-model="startDate"
+          onfocus="(this.type='date')"
+          onblur="(this.type='text')"
+        />
+      </div>
+      <BaseButton
+        class="col-auto ms-auto"
+        @click="$router.push('/certificates/selectCompany')"
+        label="ออกเอกสารรับรอง"
+      />
     </div>
 
+    <DataTable
+      :heads="tableHead"
+      :items="certificates"
+      hovers
+      clickable
+      clickReturn="cer_id"
+      @clicked=""
+      paginate
+      :total="total"
+      :active-page="page"
+      :items-per-page="pageSize"
+      @page-change="setCurrentPage"
+    >
+      <template #created_at="{ data }">
+        {{ changeTimestampToDate(data?.cer_created_at) }}
+      </template>
 
-    <DataTable :heads="tableHead" :items="certificates" hovers clickable clickReturn="cer_id" @clicked="" paginate
-        :total="total" :active-page="page" :items-per-page="pageSize" @page-change="setCurrentPage">
-        <template #created_at="{ data }">
-            {{ changeTimestampToDate(data?.cer_created_at) }}
-        </template>
+      <template #open_file="{ data }">
+        <Picture @click="openPDF(data?.cer_created_at, data?.cer_filename)"></Picture>
+      </template>
 
-        <template #open_file="{ data }">
-            <Picture @click="openPDF(data?.cer_created_at, data?.cer_filename)"></Picture>
-        </template>
-
-        <!-- <template class="col-md-2" #download="{ data }">
+      <!-- <template class="col-md-2" #download="{ data }">
             <Download @click="downloadPDF(data?.cer_created_at, data?.cer_filename)" />
         </template> -->
-        
-        <!-- <template class="col-md-2" #delete>
+
+      <!-- <template class="col-md-2" #delete>
             <Delete />
         </template> -->
-    </DataTable>
+    </DataTable></SectionSpace
+  >
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
-import BaseButton from '../Component/BaseButton.vue';
-import DataTable from '../Component/DataTable.vue';
-import Download from '../icons/DownloadButton.vue';
+import { ref, onMounted } from "vue";
+import axios from "axios";
+import BaseButton from "../Component/BaseButton.vue";
+import DataTable from "../Component/DataTable.vue";
+import Download from "../icons/DownloadButton.vue";
 import SearchBox from "../Component/SearchBox.vue";
 import BaseSelect from "../Component/BaseSelect.vue";
 import BaseInput from "../Component/BaseInput.vue";
 import Picture from "../icons/PictureLogo.vue";
-import Delete from '../icons/DeleteButton.vue';
+import Delete from "../icons/DeleteButton.vue";
 import { changeTimestampToDate } from "../../assets/js/func";
 
 const tableHead = ref([
-    { key: "cer_intern.intn_code", title: "รหัสนักศึกษาฝึกงาน", align: "center" },
-    { key: "cer_code", title: "ชื่อไฟล์", align: "center" },
-    { key: "cer_intern.work_infos[0].work_team.team_name", title: "ทีม", align: "center" },
-    { key: "cer_intern.intn_major.maj_faculty.fac_university.uni_name", title: "มหาวิทยาลัย", align: "center" },
-    { key: "created_at", title: "วันที่ออกเอกสาร", align: "center" },
-    { key: "open_file", title: "เปิดไฟล์", align: "center" },
-    // { key: "download", title: "ดาวน์โหลด", align: "center" },
-    // { key: "delete", title: "ลบ" },
+  { key: "cer_intern.intn_code", title: "รหัสนักศึกษาฝึกงาน", align: "center" },
+  { key: "cer_code", title: "ชื่อไฟล์", align: "center" },
+  { key: "cer_intern.work_infos[0].work_team.team_name", title: "ทีม", align: "center" },
+  {
+    key: "cer_intern.intn_major.maj_faculty.fac_university.uni_name",
+    title: "มหาวิทยาลัย",
+    align: "center",
+  },
+  { key: "created_at", title: "วันที่ออกเอกสาร", align: "center" },
+  { key: "open_file", title: "เปิดไฟล์", align: "center" },
+  // { key: "download", title: "ดาวน์โหลด", align: "center" },
+  // { key: "delete", title: "ลบ" },
 ]);
-const certificates = ref([])
+const certificates = ref([]);
 
 const getAllCertificate = async () => {
-
-    try {
-        const response = await axios.get(`${import.meta.env.VITE_API_HOST}/certificates`);
-        certificates.value = response.data;
-    } catch (error) {
-        console.error('Error fetching certificates:', error);
-    }
+  try {
+    const response = await axios.get(`${import.meta.env.VITE_API_HOST}/certificates`);
+    certificates.value = response.data;
+  } catch (error) {
+    console.error("Error fetching certificates:", error);
+  }
 };
 
 async function downloadPDF(year, cerFilename) {
-    window.open(`${import.meta.env.VITE_API_HOST}/certificates/${changeTimestampToYear(year)}/${cerFilename}`)
+  window.open(
+    `${import.meta.env.VITE_API_HOST}/certificates/${changeTimestampToYear(
+      year
+    )}/${cerFilename}`
+  );
 }
 
 async function openPDF(year, cerFilename) {
-    window.open(`${import.meta.env.VITE_API_HOST}/certificates/${changeTimestampToYear(year)}/${cerFilename}`)
+  window.open(
+    `${import.meta.env.VITE_API_HOST}/certificates/${changeTimestampToYear(
+      year
+    )}/${cerFilename}`
+  );
 }
 
 function changeTimestampToYear(value) {
-    if (value) {
-        const date = new Date(value)
-        let day = date.getDate();
-        let month = date.getMonth() + 1;
-        let year = date.getFullYear();
-        if (day < 10) {
-            day = 0 + `${day}`
-        }
-        if (month < 10) {
-            month = `${month}`
-        }
-        return `${year}-${month}`
+  if (value) {
+    const date = new Date(value);
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    if (day < 10) {
+      day = 0 + `${day}`;
     }
-    return ''
+    if (month < 10) {
+      month = `${month}`;
+    }
+    return `${year}-${month}`;
+  }
+  return "";
 }
 
 onMounted(async () => {
-    getAllCertificate();
-})
-
-
-
+  getAllCertificate();
+});
 </script>
 
 <style scoped>
 .search {
-    background-color: #8d969b30 !important;
+  background-color: #8d969b30 !important;
 }
 </style>
