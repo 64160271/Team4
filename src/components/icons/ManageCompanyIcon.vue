6a61f6f589e9d31f1835ea8cddb56b167304995c
64160271 <118897380+64160271@users.noreply.github.com>
2024-02-20 17:51:40 +0700

    ..

---
+++ src/components/icons/ManageCompanyIcon.vue
@@ -0,0 +1,13 @@
+<!--
+    ManageCompanyIcon
+    เป็น Icon ส่วนประกอบหลักของการจัดการบริษัท
+    โดยใช้สำหรับโชว์ที่ Navbar
+    Author : Team 4
+    Created date : 04-02-2567
+-->
+
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+        <path
+            d="M80-120v-720h400v160h400v560H80Zm80-80h80v-80h-80v80Zm0-160h80v-80h-80v80Zm0-160h80v-80h-80v80Zm0-160h80v-80h-80v80Zm160 480h80v-80h-80v80Zm0-160h80v-80h-80v80Zm0-160h80v-80h-80v80Zm0-160h80v-80h-80v80Zm160 480h320v-400H480v80h80v80h-80v80h80v80h-80v80Zm160-240v-80h80v80h-80Zm0 160v-80h80v80h-80Z" />
+</svg></template>
