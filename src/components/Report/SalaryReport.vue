a85891270bc27a7b5c9e0a5311edfb27720fb4be
Rawich Piboonsin <64160299@go.buu.ac.th>
2024-03-15 11:51:08 +0700

    success report

--- src/components/Report/SalaryReport.vue
+++ src/components/Report/SalaryReport.vue
@@ -1,83 +1,90 @@
 <template>
-  <LayoutMenuName page-name="รายงานเบี้ยเลี้ยง" />
+    <LayoutMenuName page-name="รายงานเบี้ยเลี้ยง" />
 
-  <SectionSpace>
-    <div class="row mb-3 top-300">
-      <div class="col-md-2 my-auto">
-        <BaseSelect placeholder="ปี" @change="fetchReport()" v-model="yearSelect" :options="listYear" />
-      </div>
-
-      <div class="col-md-2 my-auto">
-        <BaseSelect @change="fetchTeamReport()" placeholder="ทีม" all-select v-model="team_id" :options="teams"
-          value="team_id" text="team_name" />
-      </div>
-
-      <button class="col-md-2 btn ms-auto  outline-red" @click="downloadExcelFile">
-        ดาวน์โหลดรายงาน
-      </button>
-
-      <div class="row my-2">
-        <div class="form-check form-check-inline fw-bold">
-          รายงานค่าใช้จ่ายเบี้ยเลี้ยงสหกิจศึกษา ประจำปี : <span>{{ yearSelect }}</span>
-        </div>
-      </div>
-
-      <div class="row my-2">
-        <div class="form-check form-check-inline fw-bold">
-          ของทีม : <span>{{ teamName }}</span>
-        </div>
-      </div>
-    </div>
-
-    <div class="row mb-4 p-0" v-if="view == 'T'">
-
-      <div class="border col mx-2 my-auto py-3">
-        <div class="row mx-auto">
-          <div class="col-md-5 text-center">
-            <img height="100" src="@/assets/images/wages.png" alt="">
-          </div>
-          <div class="col my-auto">
-            <div class="text">รวมเบี้ยเลี้ยง</div>
-            <div class="money">
-              <span> {{ '฿' + totalSalary.toFixed(2) || 0 }}</span>
+    <SectionSpace>
+        <div class="row mb-3">
+            <div class="col-md-2 my-auto nopadding">
+                <BaseSelect placeholder="ปี" @change="fetchReport()" v-model="yearSelect" :options="listYear" />
             </div>
-          </div>
-        </div>
-      </div>
 
-      <div class="border col my-auto p-3 mx-2 my-auto">
-        <div class="row mx-auto">
-          <div class="col-md-5 text-center">
-            <img height="100" src="@/assets/images/extra.png" alt="">
-          </div>
-          <div class="col my-auto">
-            <div class="text">รวมเบี้ยเลี้ยงพิเศษ</div>
-            <div class="money">
-              <span> {{ '฿' + totalExtra.toFixed(2) || 0 }}</span>
+            <div class="col-md-2 my-auto">
+                <BaseSelect @change="fetchTeamReport()" placeholder="ทีม" all-select v-model="team_id" :options="teams"
+                    value="team_id" text="team_name" />
             </div>
-          </div>
-        </div>
-      </div>
 
-      <div class="border col my-auto p-3 mx-2 my-auto">
-        <div class="row mx-auto">
-          <div class="col-md-5 text-center">
-            <img height="100" src="@/assets/images/total.png" alt="">
-          </div>
-          <div class="col my-auto">
-            <div class="text">รวมเบี้ยเลี้ยงทั้งหมด</div>
-            <div class="money">
-              <span> {{ '฿' + totalAll.toFixed(2) || 0 }}</span>
+            <button class="col-md-2 btn ms-auto outline-red p-0" @click="downloadExcelFile">
+                ดาวน์โหลดรายงาน
+            </button>
+        </div>
+
+
+        <div class="row my-3">
+            <div class="fw-bold nopadding">
+                รายงานค่าใช้จ่ายเบี้ยเลี้ยงสหกิจศึกษา ประจำปี : <span>{{ yearSelect }}</span>
             </div>
-          </div>
         </div>
-      </div>
 
-    </div>
+        <div class="row my-2 mb-3">
+            <div class="fw-bold nopadding">
+                ของทีม : <span>{{ teamName || 'ทั้งหมด' }}</span>
+            </div>
+        </div>
 
-    <Bar v-if="loaded && view == 'A'" :options="optionsYear" :data="chartAllData" />
-    <Bar v-if="loaded && view == 'T'" :options="optionsTeam" :data="chartTeamData" />
-  </SectionSpace>
+        <div class="row mb-4 p-0" v-if="view == 'T'">
+
+            <div class="border col me-2 my-auto py-3">
+                <div class="row mx-auto">
+                    <div class="col-md-5 text-center">
+                        <img height="100" src="@/assets/images/wages.png" alt="">
+                    </div>
+                    <div class="col my-auto">
+                        <div class="text">รวมเบี้ยเลี้ยง</div>
+                        <div class="money">
+                            <span> {{ '฿' + totalSalary.toFixed(2) || 0 }}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="border col my-auto p-3 mx-2 my-auto">
+                <div class="row mx-auto">
+                    <div class="col-md-5 text-center">
+                        <img height="100" src="@/assets/images/extra.png" alt="">
+                    </div>
+                    <div class="col my-auto">
+                        <div class="text">รวมเบี้ยเลี้ยงพิเศษ</div>
+                        <div class="money">
+                            <span> {{ '฿' + totalExtra.toFixed(2) || 0 }}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="border col my-auto p-3 ms-2 my-auto">
+                <div class="row mx-auto">
+                    <div class="col-md-5 text-center">
+                        <img height="100" src="@/assets/images/total.png" alt="">
+                    </div>
+                    <div class="col my-auto">
+                        <div class="text">รวมเบี้ยเลี้ยงทั้งหมด</div>
+                        <div class="money">
+                            <span> {{ '฿' + totalAll.toFixed(2) || 0 }}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <Loading v-if="!loaded" />
+
+            <Bar v-if="loaded && view == 'A'" :options="optionsYear" :data="chartAllData" />
+            <Bar id="teamChart" v-if="loaded && view == 'T'" :options="optionsTeam" :data="chartTeamData"
+                :plugins="[linePosition]" />
+        </div>
+
+    </SectionSpace>
 </template>
 
 <script setup>
@@ -86,15 +93,15 @@ import BaseSelect from "../Component/BaseSelect.vue";
 import apiService from "../../services/api";
 import { Bar, Line } from "vue-chartjs";
 import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  LineElement,
-  PointElement,
-  CategoryScale,
-  LinearScale,
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    LineElement,
+    PointElement,
+    CategoryScale,
+    LinearScale,
 } from "chart.js";
 import axios from "axios";
 
@@ -103,278 +110,288 @@ const view = ref("A");
 const totalSalary = ref(0);
 const totalExtra = ref(0);
 const totalAll = ref(0);
-const name = ref("BarChart");
 const loaded = ref(false);
 const yearSelect = ref(currentYear);
 const teamName = ref("");
 
 const optionsYear = ref({
-  aspectRatio: 1.2,
-  indexAxis: 'y',
-  responsive: true,
-  scales: {
-    x: {
-      title: {
-        display: true,
-        text: "Salary",
-        font: {
-          size: 16,
-        }
-      },
+    aspectRatio: 1.2,
+    indexAxis: 'y',
+    scales: {
+        x: {
+            title: {
+                display: false,
+                text: "Salary",
+                font: {
+                    size: 16,
+                }
+            },
+        },
+        y: {
+            title: {
+                display: false,
+                text: "Teams",
+                font: {
+                    size: 16,
+                }
+            },
+        },
     },
-    y: {
-      title: {
-        display: true,
-        text: "Teams",
-        font: {
-          size: 16,
-        }
-      },
-    },
-  },
-  plugins: {
+    plugins: {
 
-    title: {
-      display: true,
-      text: "รายงานค่าใช้จ่ายเบี้ยเลี้ยงสหกิจศึกษา ประจำปี " + yearSelect.value,
-      font: {
-        size: 18,
-        weight: 'bold',
-      }
+        title: {
+            display: true,
+            text: "รายงานค่าใช้จ่ายเบี้ยเลี้ยงสหกิจศึกษา ประจำปี " + yearSelect.value,
+            font: {
+                size: 18,
+                weight: 'bold',
+            }
+        },
     },
-  },
 });
 
+const linePosition = {
+    id: 'linePosition',
+    beforeDatasetsDraw(chart) {
+        chart.getDatasetMeta(1).data.forEach((datapoint, index) => {
+            datapoint.x = chart.getDatasetMeta(0).data[index].x
+        })
+        chart.getDatasetMeta(3).data.forEach((datapoint, index) => {
+            datapoint.x = chart.getDatasetMeta(2).data[index].x
+        })
+    }
+}
+
 const optionsTeam = ref({
-  scales: {
-    x: {
-      title: {
-        display: true,
-        text: "Month",
-        font: {
-          size: 16,
-        }
-      },
+    scales: {
+        x: {
+            title: {
+                display: false,
+                text: "Month",
+                font: {
+                    size: 16,
+                }
+            },
+        },
+        y: {
+            title: {
+                display: false,
+                text: "Salary",
+                font: {
+                }
+            },
+        },
     },
-    y: {
-      title: {
-        display: true,
-        text: "Salary",
-        font: {
-        }
-      },
-    },
-  },
-  responsive: true,
-  plugins: {
-    title: {
-      display: true,
-      text: "รายงานค่าใช้จ่ายเบี้ยเลี้ยงสหกิจศึกษา ประจำปี " + yearSelect.value + " ของทีม " + teamName.value,
-      font: {
-        size: 18,
-        weight: 'bold',
-      }
-    },
-  },
+    plugins: 
+        {
+            title: {
+                display: true,
+                text: "รายงานค่าใช้จ่ายเบี้ยเลี้ยงสหกิจศึกษา ประจำปี " + yearSelect.value + " ของทีม " + teamName.value,
+                font: {
+                    size: 18,
+                    weight: 'bold',
+                }
+            },
+        },
+    
 });
 
 const chartAllData = ref({
-  labels: [],
-  datasets: [
-    {
-      label: "salary",
-      barThickness: 30,
-      data: [],
-      borderWidth: 1,
-      backgroundColor: "rgba(75, 192, 192, 0.2)",
-      borderColor: "rgba(75, 192, 192, 1)",
-    },
-    {
-      label: "extra",
-      barThickness: 30,
-      data: [],
-      borderWidth: 1,
-      backgroundColor: "rgba(255, 99, 132, 0.2)",
-      borderColor: "rgba(255, 99, 132, 1)",
-    },
-  ],
+    labels: [],
+    datasets: [
+        {
+            label: "เบี้ยเลี้ยง",
+            data: [],
+            borderWidth: 1,
+            backgroundColor: "rgba(75, 192, 192, 0.2)",
+            borderColor: "rgba(75, 192, 192, 1)",
+        },
+        {
+            label: "เบี้ยเลี้ยงพิเศษ",
+            data: [],
+            borderWidth: 1,
+            backgroundColor: "rgba(255, 99, 132, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+        },
+    ],
 });
 
 const chartTeamData = ref({
-  labels: [],
-  datasets: [
-    {
-      label: "",
-      data: [],
-      borderWidth: 1,
-      backgroundColor: "rgba(75, 192, 192, 0.2)",
-      borderColor: "rgba(75, 192, 192, 1)",
-    },
-    {
-      label: "salary",
-      data: [],
-      backgroundColor: "rgba(0, 191, 255, 0.1)",
-      borderColor: "rgba(0, 191, 255, 0.8 )",
-    },
+    labels: [],
+    datasets: [
+        {
+            label: "",
+            data: [],
+            borderWidth: 1,
+            backgroundColor: "rgba(75, 192, 192, 0.2)",
+            borderColor: "rgba(75, 192, 192, 1)",
+        },
+        {
+            type: 'line',
+            label: "เบี้ยเลี้ยง",
+            data: [],
+            backgroundColor: "rgba(0, 191, 255, 0.1)",
+            borderColor: "rgba(0, 191, 255, 0.8 )",
+        },
 
-    {
-      label: "",
-      data: [],
-      borderWidth: 1,
-      backgroundColor: "rgba(255, 99, 132, 0.2)",
-      borderColor: "rgba(255, 99, 132, 1)",
-    },
-    {
-      label: "extra",
-      data: [],
-      backgroundColor: "rgba(255, 105, 180, 0.1)",
-      borderColor: "rgba(255, 105, 180, 1)",
-    },
-  ],
+        {
+            label: "",
+            data: [],
+            borderWidth: 1,
+            backgroundColor: "rgba(255, 99, 132, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+        },
+        {
+            type: 'line',
+            label: "เบี้ยเลี้ยงพิเศษ",
+            data: [],
+            backgroundColor: "rgba(255, 105, 180, 0.1)",
+            borderColor: "rgba(255, 105, 180, 1)",
+        },
+    ],
 });
 const teams = ref([]);
 const team_id = ref();
 const listYear = ref([]);
 
 ChartJS.register(
-  Title,
-  Tooltip,
-  Legend,
-  LineElement,
-  PointElement,
-  BarElement,
-  CategoryScale,
-  LinearScale
+    Title,
+    Tooltip,
+    Legend,
+    LineElement,
+    PointElement,
+    BarElement,
+    CategoryScale,
+    LinearScale
 );
 
 const fetchAllReport = async () => {
-  view.value = "A";
-  const params = {
-    year: yearSelect.value - 543,
-  };
+    view.value = "A";
+    const params = {
+        year: yearSelect.value - 543,
+    };
 
-  loaded.value = false;
+    loaded.value = false;
 
-  const data = await axios
-    .get(`${import.meta.env.VITE_API_HOST}/salaries/teams`, { params })
-    .then((response) => {
-      return response.data;
+    const data = await axios
+        .get(`${import.meta.env.VITE_API_HOST}/salaries/teams`, { params })
+        .then((response) => {
+            return response.data;
+        });
+
+    const name = data.map((d) => {
+        return d.team_name;
+    });
+    const extra = data.map((d) => {
+        return d.sum_extra;
+    });
+    const salary = data.map((d) => {
+        return d.sum_salary;
     });
 
-  const teamName = data.map((d) => {
-    return d.team_name;
-  });
-  const extra = data.map((d) => {
-    return d.sum_extra;
-  });
-  const salary = data.map((d) => {
-    return d.sum_salary;
-  });
-
-  chartAllData.value.labels = teamName;
-  chartAllData.value.datasets[0].data = salary;
-  chartAllData.value.datasets[1].data = extra;
-  optionsYear.value.plugins.title.text = "รายงานค่าใช้จ่ายเบี้ยเลี้ยงสหกิจศึกษา ประจำปี " + yearSelect.value
-  loaded.value = true;
+    chartAllData.value.labels = name;
+    chartAllData.value.datasets[0].data = salary;
+    chartAllData.value.datasets[1].data = extra;
+    optionsYear.value.plugins.title.text = "รายงานค่าใช้จ่ายเบี้ยเลี้ยงสหกิจศึกษา ประจำปี " + yearSelect.value
+    loaded.value = true;
+    teamName.value = ""
 };
 
 const fetchTeamReport = async () => {
-  if (!team_id.value) {
-    view.value = "A";
-    fetchAllReport();
-    return;
-  }
+    if (!team_id.value) {
+        view.value = "A";
+        fetchAllReport();
+        return;
+    }
 
-  view.value = "T";
-  const params = {
-    year: yearSelect.value - 543,
-  };
+    view.value = "T";
+    const params = {
+        year: yearSelect.value - 543,
+    };
 
-  loaded.value = false;
+    loaded.value = false;
 
-  const data = await axios
-    .get(`${import.meta.env.VITE_API_HOST}/salaries/teams/${team_id.value}`, { params })
-    .then((response) => {
-      return response.data;
+    const data = await axios
+        .get(`${import.meta.env.VITE_API_HOST}/salaries/teams/${team_id.value}`, { params })
+        .then((response) => {
+            return response.data;
+        });
+
+    teamName.value = data.name;
+    const month = data.reports.map((d) => {
+        return d.month;
+    });
+    const extra = data.reports.map((d) => {
+        return d.sum_extra;
+    });
+    const salary = data.reports.map((d) => {
+        return d.sum_salary;
     });
 
-  teamName.value = data.name;
-  const month = data.reports.map((d) => {
-    return d.month;
-  });
-  const extra = data.reports.map((d) => {
-    return d.sum_extra;
-  });
-  const salary = data.reports.map((d) => {
-    return d.sum_salary;
-  });
+    chartTeamData.value.labels = month;
+    chartTeamData.value.datasets[0].data = salary;
+    chartTeamData.value.datasets[1].data = salary;
+    chartTeamData.value.datasets[2].data = extra;
+    chartTeamData.value.datasets[3].data = extra;
+    loaded.value = true;
 
-  chartTeamData.value.labels = month;
-  chartTeamData.value.datasets[0].data = salary;
-  chartTeamData.value.datasets[1].data = salary;
-  chartTeamData.value.datasets[1].type = "line";
-  chartTeamData.value.datasets[2].data = extra;
-  chartTeamData.value.datasets[3].data = extra;
-  chartTeamData.value.datasets[3].type = "line";
-  loaded.value = true;
-
-  totalSalary.value = salary.reduce((a, b) => Number(a) + Number(b));
-  totalExtra.value = extra.reduce((a, b) => Number(a) + Number(b));
-  totalAll.value = totalSalary.value + totalExtra.value;
-  optionsTeam.value.plugins.title.text = "รายงานค่าใช้จ่ายเบี้ยเลี้ยงสหกิจศึกษา ประจำปี " + yearSelect.value + " ของทีม " + teamName.value
+    totalSalary.value = salary.reduce((a, b) => Number(a) + Number(b));
+    totalExtra.value = extra.reduce((a, b) => Number(a) + Number(b));
+    totalAll.value = totalSalary.value + totalExtra.value;
+    optionsTeam.value.plugins.title.text = "รายงานค่าใช้จ่ายเบี้ยเลี้ยงสหกิจศึกษา ประจำปี " + yearSelect.value + " ของทีม " + teamName.value
 };
 
 onMounted(async () => {
-  const services = new apiService();
-  teams.value = await services.getAllTeam();
+    const services = new apiService();
+    teams.value = await services.getAllTeam();
 
-  try {
-    await fetchAllReport();
+    try {
+        await fetchAllReport();
 
-    for (let i = currentYear; i > 2550; i--) {
-      listYear.value.push(i);
+        for (let i = currentYear; i > 2550; i--) {
+            listYear.value.push(i);
+        }
+    } catch (e) {
+        console.error(e);
     }
-  } catch (e) {
-    console.error(e);
-  }
 });
 
 const fetchReport = () => {
-  if (view.value === "A" || team_id.value == "") {
-    fetchAllReport();
-  } else if (view.value === "T") {
-    fetchTeamReport();
-  }
+    if (view.value === "A" || team_id.value == "") {
+        fetchAllReport();
+    } else if (view.value === "T") {
+        fetchTeamReport();
+    }
 };
 
 const downloadExcelFile = async () => {
-  window.open(`${import.meta.env.VITE_API_HOST}/salaries/excel`);
+    window.open(`${import.meta.env.VITE_API_HOST}/salaries/excel`);
 };
 </script>
 
 <style scoped>
 .border {
-  border: 1px solid rgb(34, 34, 34);
-  border-radius: 10px;
+    border: 1px solid rgb(34, 34, 34);
+    border-radius: 8px;
 }
 
 .text {
-  height: 30px;
-  text-align: center;
+    height: 30px;
+    text-align: center;
 }
 
 .money {
-  height: 30px;
-  text-align: center;
+    height: 30px;
+    text-align: center;
 }
 
 .text {
-  height: 30px;
-  font-size: 20px;
+    height: 30px;
+    font-size: 20px;
 }
 
 .money {
-  height: 30px;
-  font-size: 16px;
+    height: 30px;
+    font-size: 16px;
 }
 </style>
